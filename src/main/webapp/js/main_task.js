d06e065ccb246b39983503f631a64d9a5db4d272
MinsikPark <pms3300@gmail.com>
2018-07-12 22:14:34 +0900

    동영상 촬영 전 css완료본

--- src/main/webapp/js/main_task.js
+++ src/main/webapp/js/main_task.js
@@ -34,7 +34,7 @@ if(inboxkind == "sent" || inboxkind == "archive" || inboxkind == "incomming"){
 			tid= temptid.substring(6);
 			console.log(tid);
 		}
-		
+        const loadingpage_third = '<div id="loading" class="loading_third"><img style="width:auto;height:100px;border-radius:50%"id="loading_img" alt="loading" src="img/rocket_3.gif" /></div>';
 		$.ajax(
 			       {
 			           type : "post",
@@ -43,8 +43,10 @@ if(inboxkind == "sent" || inboxkind == "archive" || inboxkind == "incomming"){
 			        	   'tid': tid
 			           },
 			           beforeSend:function(){
-			        	var loadinggif = '<div id="loading" class="loading_third"><img style="width:auto;height:100px;border-radius:50%"id="loading_img" alt="loading" src="img/rocket_3.gif" /></div>';
-			   			$('#right').append(loadinggif);
+                        $('.modal-content2').css('display','none');
+                        $('.modal-content3').css('display','none');
+                           $('#right').append(loadingpage_third);
+
 			   			},
 			           success : function(rdata){
 			        	   console.log('성공!')
@@ -286,6 +288,9 @@ if(inboxkind == "sent" || inboxkind == "archive" || inboxkind == "incomming"){
 			        	   $(".starred-secondbody-image").hide();
 			        		$(".modal-content2").show();
 			        		$(".modal-content3").show();
+                            $('.modal-content2').css('display','block');
+                            $('.modal-content3').css('display','block');
+
 			        		$("#loading").remove();
 			           } // end-success
 			        } 
